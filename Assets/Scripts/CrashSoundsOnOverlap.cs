64c127c6c181fe8f6cbd3022dd9781b3d4a455b0
pyoneerC <maxcomperatutti@gmail.com>
2024-07-10 17:38:49 -0300

    crash sound on overlap comments

--- Assets/Scripts/CrashSoundsOnOverlap.cs
+++ Assets/Scripts/CrashSoundsOnOverlap.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+/// <summary>
+/// This script plays a random crash sound when the car collides with the ground.
+/// </summary>
 public class CrashSoundsOnOverlap : MonoBehaviour
 {
     public AudioSource crashSound1;
@@ -7,6 +10,13 @@ public class CrashSoundsOnOverlap : MonoBehaviour
     public AudioSource crashSound3;
     public AudioSource crashSound4;
 
+    /// <summary>
+    /// Plays a random crash sound when the car collides with the ground.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns>
+    /// void
+    /// </returns>
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Ground")) return;
@@ -25,6 +35,9 @@ public class CrashSoundsOnOverlap : MonoBehaviour
                 break;
             case 3:
                 crashSound4.Play();
+                break;
+            default:
+                Debug.LogError("WHAT?! How did you get here? This should never happen!");
                 break;
         }
 
